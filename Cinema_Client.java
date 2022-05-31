501ccee5f638da6bf5557e9b29579f1cf6d5103d
Daniel Condarcuri <81829209+CDannyC@users.noreply.github.com>
2022-05-31 13:28:58 -0400

    Add files via upload

--- Cinema_Client.java
+++ Cinema_Client.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
+import java.text.DecimalFormat;
 
 public class Cinema_Client 
 {
     //public static int userSelect;
+    public static DecimalFormat df = new DecimalFormat("#,###,##0.00");
     public static int ticketCounter;
     public static Cinema cineplex = new Cinema();
     public static double balance;
@@ -15,7 +17,6 @@ public class Cinema_Client
         //Object a = dropDownMenu(cineplex.getMovies());
         //System.out.println(a);
 
-        String test1= JOptionPane.showInputDialog("");
 
         while(true)
         {
@@ -28,18 +29,18 @@ public class Cinema_Client
             }
             else if (userSelection == 1)
             {
-                
+                addMoney();
             }
             else
             {
                 System.exit(0);
             }
             System.out.println(cineplex.getItems());
-        }   
+        }//end loop
 
         
-    }
-    public static double movieSelect()
+    }//end main
+    public static void movieSelect()
     {
         String[] menuOption = {"Adult Ticket", "Child Ticket"};
 
@@ -53,7 +54,7 @@ public class Cinema_Client
         {
             a = cineplex.getMovies().get(i);
             arr[i] = a.getTitle();
-        }
+        }//end loop
         
         Object selection = JOptionPane.showInputDialog(null, "choose type", "selection", JOptionPane.QUESTION_MESSAGE, null, arr, arr[0]);
 
@@ -66,41 +67,43 @@ public class Cinema_Client
                 mov = cineplex.getMovies().get(i);
                 if (b == 0)
                 {
-                    cineplex.getItems().addItem(new Ticket(cineplex.getMovies().get(i).getCost(), cineplex.getMovies().get(i)));
+                    if (JOptionPane.showConfirmDialog(null, "Add adult ticket for " + df.format(cineplex.getMovies().get(i).getCost()) + " to see " + arr[i], "Add Item", JOptionPane.YES_NO_OPTION) == 0)
+                    {
+                        cineplex.getItems().addItem(new Ticket(cineplex.getMovies().get(i).getCost(), cineplex.getMovies().get(i)));
+                    }
                 }
                 else if (b == 1)
                 {
-                    cineplex.getItems().addItem(new Ticket(cineplex.getMovies().get(i).getChildCost(), cineplex.getMovies().get(i)));
+                    if (JOptionPane.showConfirmDialog(null, "Add child ticket for " + df.format(cineplex.getMovies().get(i).getChildCost()) + " to see " + arr[i], "Add Item", JOptionPane.YES_NO_OPTION) == 0)
+                    {
+                        cineplex.getItems().addItem(new Ticket(cineplex.getMovies().get(i).getChildCost(), cineplex.getMovies().get(i)));
+                    }
                 }
                 else
                 {
                     System.exit(0);
                 }
-            }
-        }
+            }//end if
+        }//end loop
         
 
         
         //int b = JOptionPane.showOptionDialog(JOptionPane.getRootFrame(), "hello", "title", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, menuOption, menuOption[1]);
 
-        if (b == 0)
-        {
-            
-        }
 
-        return 0;
-    }
+    }//end movieSelect()
 
     public static void addMoney()
     {
-        int[] vals = {5, 10 , 25, 50, 100};
-        int selection = JOptionPane.showInputDialog(null, "choose type", "selection", JOptionPane.QUESTION_MESSAGE, null, vals, vals[0]);
-        balance += vals[selection]; 
-    }
+        Integer[] vals = {5, 10 , 25, 50, 100};
+        Object selection = JOptionPane.showInputDialog(null, "Select amount of money to add to your balance", "Balance", JOptionPane.QUESTION_MESSAGE, null, vals, vals[0]);
+        balance += (int)selection;
+        JOptionPane.showMessageDialog(null, "Your current balance is: $" + df.format(balance), "Balance", JOptionPane.PLAIN_MESSAGE);
+    }//end addMoney()
 
     public static int mainMenu()
     {
         String[] menuOption = {"Purchase Movie Tickets", "Add Money", "exit"}; 
         return JOptionPane.showOptionDialog(JOptionPane.getRootFrame(), "hello", "title", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, menuOption, menuOption[1]);    
-    }
-}
+    }//end mainMenu()
+}//end class
