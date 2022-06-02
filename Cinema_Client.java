0896d5f7ee5351a58400895de3b12c36ef10a60c
Daniel Condarcuri <81829209+CDannyC@users.noreply.github.com>
2022-06-02 13:28:19 -0400

    Add files via upload

--- Cinema_Client.java
+++ Cinema_Client.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public class Cinema_Client 
 {
@@ -7,7 +8,7 @@ public class Cinema_Client
     public static DecimalFormat df = new DecimalFormat("#,###,##0.00");
     public static int ticketCounter;
     public static Cinema cineplex = new Cinema();
-    public static double balance;
+    public static double balance = 100;
     public static void main(String args[])
     {
         int userSelection;
@@ -41,7 +42,7 @@ public class Cinema_Client
 
         
     }//end main
-    public static void movieSelect()
+    public static int movieSelect()
     {
         String[] menuOption = {"Adult Ticket", "Child Ticket"};
 
@@ -51,13 +52,18 @@ public class Cinema_Client
         Movie mov;
 
         
-        for (int i = 0; i < cineplex.getMovies().size();i++)
+        for (int i = 0; i < cineplex.getMovies().size(); i++)
         {
             a = cineplex.getMovies().get(i);
             arr[i] = a.getTitle();
         }//end loop
         
         Object selection = JOptionPane.showInputDialog(null, "choose type", "selection", JOptionPane.QUESTION_MESSAGE, null, arr, arr[0]);
+        System.out.println(selection);
+        System.out.println(selection == null);
+        if (selection == null)
+        { return -1;}
+        //if (selection.equals())
 
         int b = JOptionPane.showOptionDialog(JOptionPane.getRootFrame(), "hello", "title", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, menuOption, menuOption[1]);
 
@@ -70,14 +76,33 @@ public class Cinema_Client
                 {
                     if (JOptionPane.showConfirmDialog(null, "Add adult ticket for " + df.format(cineplex.getMovies().get(i).getCost()) + " to see " + arr[i], "Add Item", JOptionPane.YES_NO_OPTION) == 0)
                     {
-                        cineplex.getItems().addItem(new Ticket(cineplex.getMovies().get(i).getCost(), cineplex.getMovies().get(i)));
+                        if (balance >= cineplex.getMovies().get(i).getCost())
+                        {
+                            balance -= cineplex.getMovies().get(i).getCost();
+                            cineplex.getItems().addItem(new Ticket(cineplex.getMovies().get(i).getCost(), cineplex.getMovies().get(i)));
+                        }
+                        else
+                        {
+                            JOptionPane.showConfirmDialog(null, "Insufficient Funds!", "TBD", JOptionPane.OK_OPTION);
+                            return -1;
+                        }
                     }
                 }
                 else if (b == 1)
                 {
                     if (JOptionPane.showConfirmDialog(null, "Add child ticket for " + df.format(cineplex.getMovies().get(i).getChildCost()) + " to see " + arr[i], "Add Item", JOptionPane.YES_NO_OPTION) == 0)
                     {
-                        cineplex.getItems().addItem(new Ticket(cineplex.getMovies().get(i).getChildCost(), cineplex.getMovies().get(i)));
+                        if (balance >= cineplex.getMovies().get(i).getChildCost())
+                        {
+                            balance -= cineplex.getMovies().get(i).getChildCost();
+                            cineplex.getItems().addItem(new Ticket(cineplex.getMovies().get(i).getChildCost(), cineplex.getMovies().get(i)));
+                        }
+                        else
+                        {
+                            JOptionPane.showConfirmDialog(null, "Insufficient Funds!", "TBD", JOptionPane.OK_OPTION);
+                            return -1;
+                        }
+
                     }
                 }
                 else
@@ -91,7 +116,7 @@ public class Cinema_Client
         
         //int b = JOptionPane.showOptionDialog(JOptionPane.getRootFrame(), "hello", "title", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, menuOption, menuOption[1]);
 
-
+        return 0;
     }//end movieSelect()
 
     public static void addMoney()
@@ -105,20 +130,50 @@ public class Cinema_Client
     public static int viewCart()
     {
         int selection;
+        String str;
+        boolean isChecked;
         Object[] arr = new Object[cineplex.getItems().getCart().size()];
         String[] menuOption = {"Void Item", "Go Back"};
+        JCheckBox[] checkB = new JCheckBox[cineplex.getItems().getCart().size()];
+        ArrayList<Integer> rmv = new ArrayList<Integer>();
         
         for (int i = 0; i < cineplex.getItems().getCart().size(); i++)
         {
+            checkB[i] = new JCheckBox(cineplex.getItems().getItem(i).toString());
             arr[i] = cineplex.getItems().getItem(i);
         }
 
         selection = JOptionPane.showOptionDialog(null, cineplex.getItems() , "Your Cart",JOptionPane.DEFAULT_OPTION,JOptionPane.PLAIN_MESSAGE,null, menuOption, menuOption[1]);
         if (selection == 0)
         {
+
             try
             {
-                JOptionPane.showInputDialog(null, "Select item to void: ", "selection", JOptionPane.QUESTION_MESSAGE, null, arr, arr[0]);
+                if (checkB[0].equals(null))
+                {}
+                //System.out.println(JOptionPane.showConfirmDialog(JOptionPane.getRootFrame(), checkB, "Select the items you would like to void", JOptionPane.OK_CANCEL_OPTION));
+                if (JOptionPane.showConfirmDialog(JOptionPane.getRootFrame(), checkB, "Select the items you would like to void", JOptionPane.OK_CANCEL_OPTION) == 0)
+                {
+
+                    System.out.println(checkB.length);
+                    System.out.println(cineplex.getItems().getCart().size());
+                    //System.out.println(cineplex.getItems().getItem(i));
+                    System.out.println();
+                    for (int i = 0; i < checkB.length; i++) 
+                    {   
+                        System.out.println(cineplex.getItems().getItem(i));
+                        if (checkB[i].isSelected() == true)
+                        {
+                            rmv.add(i);
+                        }
+                        System.out.println(i);
+                    }
+                    for (int i = rmv.size()-1; i>=0; i--) 
+                    {
+                        cineplex.getItems().removeItem(rmv.get(i));
+                    }
+                }
+                return 1;
             }
             catch (ArrayIndexOutOfBoundsException e)
             {
@@ -131,8 +186,9 @@ public class Cinema_Client
 
     public static int mainMenu()
     {
-
+        //Math.round(a * 100.0) / 100.0;
+        String[] txt = {"Welcome To Cineplex", " ", "Current balance: $" +df.format(balance)};
         String[] menuOption = {"Purchase Movie Tickets", "Add Money", "View Cart", "exit"}; 
-        return JOptionPane.showOptionDialog(JOptionPane.getRootFrame(), "hello", "title", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, menuOption, menuOption[1]);    
+        return JOptionPane.showOptionDialog(JOptionPane.getRootFrame(), txt, "title", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, menuOption, menuOption[0]);    
     }//end mainMenu()
 }//end class
--- Movie.java
+++ Movie.java
@@ -45,5 +45,4 @@ public class Movie
                 + ", title=" + title + "]";
     }
 
-    
 }
